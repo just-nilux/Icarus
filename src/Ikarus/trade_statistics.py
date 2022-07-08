34d256c417feb9245ee3ac123b579cb042b0c8ee
Bilgehan Kösem <bilkos92@gmail.com>
2022-07-09 00:13:32 +0200

    performance.py renamed as trade_statistics.py and cleaned up

--- src/Ikarus/trade_statistics.py
+++ src/Ikarus/trade_statistics.py
@@ -8,29 +8,13 @@ import asyncio
 from datetime import datetime, timezone
 from tabulate import tabulate
 from .utils import safe_substract
+import pandas as pd
 
 
-# You can not manage something that you can not measure
-
-'''
+''' You can not manage something that you can not measure '''
 
 
-def tabulate_stats(self):    
-    f = open(self.statfile+"/stat.txt",'w')
-    for key, item in self.stat.items():
-        if type(item) != list:
-            f.write(key + "\n" )
-            f.write(tabulate(item.items()))
-            f.write("\n\n")
-
-        else:
-            f.write(tabulate(item, headers='firstrow', tablefmt='grid'))
-            f.write("\n\n")
-            
-    f.close()
-
-
-async def eval_hto_stat(self, hist_trade):
+async def eval_hto_stat(hist_trade):
 
     hto_stat = [ 
         hist_trade._id,
@@ -43,24 +27,26 @@ async def eval_hto_stat(self, hist_trade):
     ]
     return hto_stat
 
-'''
+
 
 async def eval_balance_stats(stats, mongo_client):
     start_obs = await mongo_client.get_n_docs('observer', {'type':'balance'}, order=1, n=2) # pymongo.ASCENDING
     end_obs = await mongo_client.get_n_docs('observer', {'type':'balance'}, order=-1) # pymongo.ASCENDING
 
-    start_dt = datetime.fromtimestamp(start_obs[1]['timestamp']/1000, timezone.utc)
-    end_dt = datetime.fromtimestamp(end_obs[0]['timestamp']/1000, timezone.utc)
-
-    #stats['Start Time'] = str(start_dt)
-    #stats['End Time'] = str(end_dt)
-    #stats['Total Time'] = "{:.2f} days".format((end_dt - start_dt).total_seconds() / (60*60*24)), # In second
     balance_stat = {}
     balance_stat['Start Balance'] = next(float(item['free'] + item['locked']) for item in start_obs[0]['balances'] if item['asset'] == config['broker']['quote_currency'])
-    balance_stat['End Balance'] = next(float(item['free'] + item['locked']) for item in end_obs[0]['balances'] if item['asset'] == config['broker']['quote_currency'])
-    balance_stat['Total Profit'] = (balance_stat['End Balance'] - balance_stat['Start Balance'])*100/balance_stat['Start Balance']
-    stats['Balance'] = balance_stat
     
+    open_trade_pipe = [
+        {"$group": {"_id": '', "sum": {"$sum": '$enter.amount'}}},
+    ]
+    open_trade_amount = await mongo_client.do_find("live-trades", open_trade_pipe)
+
+    balances = pd.DataFrame(end_obs[0]['balances'])
+    balances.set_index('asset', inplace=True)
+    balance_stat['End Balance'] = balances.loc[config['broker']['quote_currency']]['free'] + open_trade_amount['sum']
+    balance_stat['Absolute Profit'] = balance_stat['End Balance'] - balance_stat['Start Balance']
+    balance_stat['Percentage Profit'] = balance_stat['Absolute Profit']*100/balance_stat['Start Balance']
+    stats['Balance'] = balance_stat
 
 
 async def eval_strategy_stats(stats, mongo_client):
@@ -68,7 +54,6 @@ async def eval_strategy_stats(stats, mongo_client):
     stats['Strategies'] = {}
     for strategy in config['strategy'].keys():
         strategy_stat = {}
-        strategy_stat['Name'] = strategy
         strategy_stat['Live Trades'] = int(await mongo_client.count("live-trades", {'strategy':strategy}))
         strategy_stat['Closed'] = int(await mongo_client.count("hist-trades", {'strategy':strategy, 'result.cause':ECause.CLOSED}))
         strategy_stat['Enter Expired'] = int(await mongo_client.count("hist-trades", {'strategy':strategy, 'result.cause':ECause.ENTER_EXP}))
@@ -96,22 +81,37 @@ async def eval_strategy_stats(stats, mongo_client):
         stats['Strategies'][strategy] = strategy_stat
 
 
+# TODO: Not Tested Yet
+def tabulate_stats(stats, filename):
+    f = open(filename,'w')
+    for key, item in stats.items():
+        if type(item) != list:
+            f.write(key + "\n" )
+            f.write(tabulate(item.items()))
+            f.write("\n\n")
+
+        else:
+            f.write(tabulate(item, headers='firstrow', tablefmt='grid'))
+            f.write("\n\n")
+            
+    f.close()
+
+
 async def main():
     mongo_client = mongo_utils.MongoClient(config['mongodb']['host'], 
         config['mongodb']['port'], 
         config['tag'],
         clean=False)
     stats = {}
-    now = datetime.now()
-    now.strftime("%d/%m/%Y %H:%M:%S")
-    stats['date'] = now.strftime("%d/%m/%Y %H:%M:%S")
+    stats['Generation Date'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    stats['Start Time'] = config['backtest']['start_time']
+    stats['End Time'] = config['backtest']['end_time']
     await eval_balance_stats(stats, mongo_client)
     await eval_strategy_stats(stats, mongo_client)
     
-    f = open(os.path.dirname(str(sys.argv[1])) + "/stat.txt",'w')
+    f = open(os.path.dirname(str(sys.argv[1])) + "/stats.json",'w')
     json.dump(stats, f,  indent=4)
     f.close()
-
 
     pass
 
@@ -124,7 +124,5 @@ if __name__ == "__main__":
     with open(config['credential_file'], 'r') as cred_file:
         cred_info = json.load(cred_file)
 
-
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
