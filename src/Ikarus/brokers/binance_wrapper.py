cc2eb58e2283ae690b4cbb0275db07556e8af988
Bilgehan Kösem <bilkos92@gmail.com>
2023-02-24 00:12:07 +0100

    IC-47 IC-79 implement limit buy and sell instant fill functions; add get open orders etc api calls to test_order_execution

--- src/Ikarus/brokers/binance_wrapper.py
+++ src/Ikarus/brokers/binance_wrapper.py
@@ -14,6 +14,7 @@ from ..objects import Trade, OCO, ECause, ECommand, EState, Limit, Market, Trade
 from ..utils import setup_logger
 from dataclasses import asdict
 from binance import AsyncClient
+from ..strategies.StrategyBase import StrategyBase
 
 logger = logging.getLogger('app')
 
@@ -279,7 +280,7 @@ class BinanceWrapper():
                 price=avg_price,
                 quantity=float(response["executedQty"]),
                 fee=fee_sum,
-                cause=ECause.MARKET)
+                cause=ECause.LIMIT)
             logger.debug(f'trade.result: \n{json.dumps(asdict(trade.result), indent=4)}')
 
             return True
@@ -790,6 +791,31 @@ async def test_order_execution():
     
     client = await AsyncClient.create(**cred_info['Binance']['Test'])
     broker_client = BinanceWrapper(client, config, None)
+
+
+    symbol_info = await broker_client.get_all_symbol_info(['BTCUSDT'])
+    await test_limit_buy_instant_fill(broker_client, symbol_info)
+    await test_limit_sell_instant_fill(broker_client, symbol_info)
+    #await test_oco_sell_instant_fill(broker_client, symbol_info)
+    #await test_oco_sell_instant_stoploss_fill(broker_client, symbol_info)
+
+    orders = await client.get_open_orders(symbol='BTCUSDT')
+    logger.debug(f'client.get_open_orders: \n{orders}')
+
+    orders = await client.get_all_orders(symbol='BTCUSDT')
+    logger.debug(f'client.get_all_orders: \n{orders}')
+
+    try:
+        #fees = await client.get_trade_fee()
+        fees = await client.get_trade_fee(symbol='BNBBTC')
+        logger.debug(f'client.get_trade_fee: \n{fees}')
+
+        log = await client.get_dust_log()
+        logger.debug(f'client.get_dust_log: \n{log}')
+
+    except Exception as e:
+        logger.debug(f'client.get_trade_fee: \n{e}')
+
     df = await broker_client.get_current_balance()
     logger.info(f'balance: \n{df.to_string()}')
     trade = await test_market_buy(broker_client)
@@ -913,7 +939,6 @@ async def test_oco_sell(broker_client: BinanceWrapper):
         logger.debug('order:\n'+json.dumps(order, indent=4))
     
     return trade
-    
 
 
 async def test_order_cancel(broker_client: BinanceWrapper, trade: Trade):
@@ -921,6 +946,108 @@ async def test_order_cancel(broker_client: BinanceWrapper, trade: Trade):
     exec_status = await broker_client._execute_cancel(trade)
     assert exec_status == True
     return trade
+
+
+async def test_limit_buy_instant_fill(broker_client: BinanceWrapper, symbol_info: dict):
+    pair = 'BTCUSDT'
+    # 1. Get current price
+    prices = await broker_client.get_all_tickers()
+    current_price = float(prices.loc[pair,'price'])
+    logger.debug(f'current_price for pair {pair}: {current_price}')
+
+    # 2. Put limit order that is higher than current price
+    target_buy_price = safe_multiply(current_price, 1.102)
+    strategy_name = list(broker_client.config['strategy'].keys())[0]
+    trade = Trade(1557705600000, strategy_name, pair, EState.WAITING_ENTER, 
+        enter=Limit(amount=100, price=target_buy_price))
+    trade.result = TradeResult()
+    
+    StrategyBase.apply_exchange_filters(trade.enter, symbol_info[pair])
+
+    exec_status = await broker_client._execute_limit_buy(trade)
+    assert exec_status == True
+
+    orders = await broker_client.get_trade_orders([trade])
+    if len(orders):
+        logger.debug('order:\n'+json.dumps(list(orders.values())[0], indent=4))
+    
+    return trade
+
+
+async def test_limit_sell_instant_fill(broker_client: BinanceWrapper, symbol_info: dict):
+    pair = 'BTCUSDT'
+    # 1. Get current price
+    prices = await broker_client.get_all_tickers()
+    current_price = float(prices.loc[pair,'price'])
+    logger.debug(f'current_price for pair {pair}: {current_price}')
+
+    # 2. Put limit order that is higher than current price
+    target_sell_price = safe_multiply(current_price, 0.998)
+    strategy_name = list(broker_client.config['strategy'].keys())[0]
+    trade = Trade(1557705600000, strategy_name, pair, EState.WAITING_EXIT, 
+        exit=Limit(quantity=0.01, price=target_sell_price))
+    result_enter = Result(price=10000, amount=100, fee=0)
+    trade.result = TradeResult(enter=result_enter)
+
+    StrategyBase.apply_exchange_filters(trade.exit, symbol_info[pair])
+
+    exec_status = await broker_client._execute_limit_sell(trade)
+    assert exec_status == True
+
+    orders = await broker_client.get_trade_orders([trade])
+    if len(orders):
+        logger.debug('order:\n'+json.dumps(list(orders.values())[0], indent=4))
+    
+    return trade
+
+
+async def test_oco_sell_instant_fill_limit_maker(broker_client: BinanceWrapper, symbol_info: dict):
+    pair = 'BTCUSDT'
+    # 1. Get current price
+    prices = await broker_client.get_all_tickers()
+    current_price = float(prices.loc[pair,'price'])
+    logger.debug(f'current_price for pair {pair}: {current_price}')
+
+    # 2. Put limit order that is higher than current price
+    target_limit_maker_price = safe_multiply(current_price, 0.998)
+    target_stop_price = safe_multiply(current_price, 0.995)
+    target_stop_limit_price = safe_multiply(current_price, 0.994)
+
+    strategy_name = list(broker_client.config['strategy'].keys())[0]
+
+    exit_order = OCO(
+        price=target_limit_maker_price,
+        quantity=0.005,
+        stop_price=target_stop_price,
+        stop_limit_price=target_stop_limit_price
+    )
+
+    trade = Trade(1557705600000, strategy_name, pair, EState.WAITING_EXIT, 
+        exit=exit_order)
+    result_enter = Result(price=10000, amount=100, fee=0)
+    trade.result = TradeResult(enter=result_enter)
+
+    StrategyBase.apply_exchange_filters(trade.exit, symbol_info[pair])
+
+    exec_status = await broker_client._execute_oco_sell(trade)
+    assert exec_status == True
+
+    orders = await broker_client.get_trade_orders([trade])
+    if not len(orders):
+        return trade
+
+    for order in orders.values():
+        logger.debug('order:\n'+json.dumps(order, indent=4))
+    
+    return trade
+
+
+async def test_oco_sell_instant_fill_stop_loss(broker_client: BinanceWrapper, symbol_info: dict):
+    # TODO:
+    # 1. Get current price
+    # 2. Put limit order that is lower than current price
+    # 3. Then check the execution
+    return 
 
 
 if __name__ == '__main__':
